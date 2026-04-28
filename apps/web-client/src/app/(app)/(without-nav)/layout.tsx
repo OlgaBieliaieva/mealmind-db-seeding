d623d239e02d653958acd93cba2d5e3848747d5c
Olha Shapoval <belyaevaolya0@gmail.com>
2026-04-28 12:43:59 +0300

    fixed layout

--- apps/web-client/src/app/(app)/(without-nav)/layout.tsx
+++ apps/web-client/src/app/(app)/(without-nav)/layout.tsx
@@ -1,12 +1,9 @@
 import { ReactNode } from "react";
-import BottomNav from "@/shared/ui/navigation/BottomNav";
 
 export default function AppLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen bg-gray-50 pb-24">
       <main className="mx-auto max-w-md px-1 py-1">{children}</main>
-
-      <BottomNav />
     </div>
   );
 }
